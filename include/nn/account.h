3765a6afa665dc9e787a50e46e70614a0b88ff4f
Léo Lam <leo@leolam.fr>
2020-09-25 01:11:47 +0200

    account: Add some types and functions that are used in BotW

--- include/nn/account.h
+++ include/nn/account.h
@@ -11,25 +11,36 @@
 namespace nn {
 namespace account {
 typedef char Nickname[0x21];
-typedef u64 Uid[0x2];
 typedef u64 NetworkServiceAccountId;
 
 class AsyncContext;
-struct UserHandle;
+
+class Uid {
+public:
+    u64 m_Storage[2];
+};
+
+class UserHandle {
+public:
+    Uid m_Uid;
+    void* m_Handle;
+};
 
 void Initialize();
-Result ListAllUsers(s32*, nn::account::Uid*, s32 numUsers);
-Result OpenUser(nn::account::UserHandle*, nn::account::Uid const&);
-Result IsNetworkServiceAccountAvailable(bool* out, nn::account::UserHandle const&);
-void CloseUser(nn::account::UserHandle const&);
+Result ListAllUsers(s32*, Uid*, s32 numUsers);
+Result OpenUser(UserHandle*, Uid const&);
+Result IsNetworkServiceAccountAvailable(bool* out, UserHandle const&);
+void CloseUser(UserHandle const&);
 
-Result EnsureNetworkServiceAccountAvailable(nn::account::UserHandle const& userHandle);
-Result EnsureNetworkServiceAccountIdTokenCacheAsync(nn::account::AsyncContext*,
-                                                    nn::account::UserHandle const&);
-Result LoadNetworkServiceAccountIdTokenCache(u64*, char*, u64, nn::account::UserHandle const&);
+Result EnsureNetworkServiceAccountAvailable(UserHandle const& userHandle);
+Result EnsureNetworkServiceAccountIdTokenCacheAsync(AsyncContext*, UserHandle const&);
+Result LoadNetworkServiceAccountIdTokenCache(u64*, char*, u64, UserHandle const&);
 
-Result GetLastOpenedUser(nn::account::Uid*);
-Result GetNickname(nn::account::Nickname* nickname, nn::account::Uid const& userID);
+Result GetLastOpenedUser(Uid*);
+Result GetNickname(Nickname* nickname, Uid const& userID);
+
+Result GetUserId(Uid* uid, const UserHandle& handle);
+Result OpenPreselectedUser(UserHandle* handle);
 
 class AsyncContext {
 public:
