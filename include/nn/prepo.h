03c5553e8b25855595ffc153f909cafdd91d5b46
Léo Lam <leo@leolam.fr>
2020-09-25 14:38:21 +0200

    prepo: Add functions and types that are used by applications

--- include/nn/prepo.h
+++ include/nn/prepo.h
@@ -2,17 +2,59 @@
 
 #include "types.h"
 
-namespace nn {
-namespace prepo {
+namespace nn::account {
+class Uid;
+}
+
+namespace nn::prepo {
+
+namespace detail {
+class PlayReportGenerator {
+public:
+    PlayReportGenerator() = default;
+    void Initialize();
+
+private:
+    void* m_Data = nullptr;
+};
+}  // namespace detail
+
+enum class TransmissionStatus {};
+
 class PlayReport {
 public:
     PlayReport();
-    void SetEventId();
-    void SetBuffer();
+    explicit PlayReport(const char* event_id);
 
-    void Add(char const*, long);
-    void Add(char const*, double);
-    void Add(char const*, char const*);
+    void SetEventId(const char* event_id);
+    void SetBuffer(void* buffer, size_t size);
+    void Clear();
+
+    void Add(const char* key, s64 value);
+    void Add(const char* key, f64 value);
+    void Add(const char* key, const char* value);
+
+    Result Save();
+    Result Save(const account::Uid& uid);
+
+    s32 GetCount() const;
+
+private:
+    char m_EventId[32];
+    void* m_Buffer;
+    size_t m_BufferSize;
+    detail::PlayReportGenerator m_Generator;
 };
-};  // namespace prepo
-};  // namespace nn
+
+void Initialize();
+
+Result RequestImmediateTransmission();
+Result GetTransmissionStatus(TransmissionStatus* status);
+
+Result ClearStorage();
+Result SetOperationMode(s64 mode);
+Result IsUserAgreementCheckEnabled(bool* enabled);
+Result SetUserAgreementCheckEnabled(bool enabled);
+Result GetStorageUsage(s64*, s64*);
+
+}  // namespace nn::prepo
